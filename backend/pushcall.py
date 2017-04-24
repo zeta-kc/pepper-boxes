55a641ae30381117214664673b74c358caab330d
y0k0ta19 <motoyk@gmail.com>
2017-04-24 16:41:29 +0900

    rename pb to pushb

--- backend/pushcall.py
+++ backend/pushcall.py
@@ -11,6 +11,6 @@ def notify():
     """
     push call notification.
     """
-    pb = Pushbullet("o.9GpEBJ3D7pQzR5nAmEm3Fq5ywX3f2S50")
-    push = pb.push_note("This is the title", "This is the body")
-    print(push)
+    pushb = Pushbullet("o.9GpEBJ3D7pQzR5nAmEm3Fq5ywX3f2S50")
+    push = pushb.push_note("This is the title", "This is the body")
+    print push
